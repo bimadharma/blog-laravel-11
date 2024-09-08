f8fcf5671fc43eb103f95c76afe695a1c54af29c
bimadharma <bimadharmayahya@gmail.com>
2024-09-08 23:47:47 +0700

    article single page

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,15 +4,15 @@
   @foreach ($posts as $post)
 
   <article class="py-8 max-w-screen-md border-b border-gray-300">
-    <a href="post/{{$post['slug']}}" class="hover:underline">
-  <h2 class="mb1 text-3xl tracking-tight font-bold text-gray-900">{{$post ['title']}}</h2>
+    <a href="posts/{{$post['slug']}}" class="hover:underline">
+      <h2 class="mb1 text-3xl tracking-tight font-bold text-gray-900">{{$post ['title']}}</h2>
     </a>
-  <div class="text-base text-gray-500">
+    <div class="text-base text-gray-500">
       <a href="#">{{$post ['author']}}</a>| 9 Septemer 2024
     </div>
     <p class="my-4 font-light">{{Str::limit($post ['body']),100}}</p>
     <a href="/posts/{{$post['slug']}}" class="font-medium- text-blue-500 hover:underline">Read more &raquo;</a>
   </article>
- @endforeach
+  @endforeach
 
 </x-layout>
