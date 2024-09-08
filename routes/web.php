959be34c612ef9b438834b5201f2e461916e9556
bimadharma <bimadharmayahya@gmail.com>
2024-09-08 23:47:11 +0700

    article single

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,9 +11,51 @@ Route::get('/about', function () {
     return view('about',['name' => 'Bima Dharma Yahya','title' => 'about']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'Judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Bima Dharma Yahya',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo enim numquam dolores eos quam quo aliquid! Quo, odio consequuntur dolores veritatis voluptatum at temporibus similique, magnam architecto, doloribus suscipit repellendus?'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'Judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Bima Dharma Yahya',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas optio a ratione quidem suscipit, officiis distinctio blanditiis maiores nobis dolorem quos voluptate cupiditate pariatur tempore in, odit veritatis alias doloremque?'
+        ],
+    ]]);
 });
+
+Route::get('/posts/{slug}', function($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'Judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Bima Dharma Yahya',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo enim numquam dolores eos quam quo aliquid! Quo, odio consequuntur dolores veritatis voluptatum at temporibus similique, magnam architecto, doloribus suscipit repellendus?'
+        ],
+        [
+            
+            'id' => 2,
+            'slug' => 'Judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Bima Dharma Yahya',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas optio a ratione quidem suscipit, officiis distinctio blanditiis maiores nobis dolorem quos voluptate cupiditate pariatur tempore in, odit veritatis alias doloremque?'
+        ],
+    ];
+
+    $post = Arr::first($posts, function($post) use ($slug){
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single post', 'post' => $post]);
+});
+
 Route::get('/contact', function () {
     return view('contact', ['title' => 'contact']);
 });
