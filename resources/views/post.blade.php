202713da3558dcfec481998ad8ef8ed9f7ade1ab
bimadharma <bimadharmayahya@gmail.com>
2024-09-12 21:37:17 +0700

    Category article

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,8 +5,12 @@
   <article class="py-8 max-w-screen-md">
   <a href="/posts"><h2 class="mb1 text-3xl tracking-tight font-bold text-gray-900">{{$post ['title']}}</h2>
     </a>
-    <div class="text-base text-gray-500">
-      <a href="/authors/{{$post->author->id}}">{{$post->author->name}}</a>| {{ $post->created_at->diffForHumans()}}
+    <div>
+    By 
+      <a href="/authors/{{$post->author->username}}" class="text-base text-gray-500 hover:underline">{{$post->author->name}}</a>
+      in
+      <a href="/categories/{{$post->category->slug}}" class="text-base text-gray-500 hover:underline"> {{$post->category->name}}</a>
+      | {{ $post->created_at->diffForHumans()}}
     </div>
     <p class="my-4 font-light">{{($post ['body'])}}</p>
     <a href="/posts" class="font-medium- text-blue-500 hover:underline">&laquo; Back to posts</a>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\post;
 use App\Models\User;
+use App\Models\Category;
 use illuminate\Support\Arr;
 use illuminate\support\Facades\Route;
 
@@ -29,9 +30,13 @@ Route::get('/posts/{post:slug}', function(post $post) {
     return view('post', ['title' => 'Single post', 'post' => $post]);
 });
 
-Route::get('/authors/{user}', function(User $user) {
+Route::get('/authors/{user:username}', function(User $user) {
     
-    return view('posts', ['title' => 'Articles by '. $user->name, 'posts' => $user->posts]);
+    return view('posts', ['title' => count($user->posts) . ' Articles by '. $user->name, 'posts' => $user->posts]);
+});
+Route::get('/categories/{category:slug}', function(Category $category) {
+    
+    return view('posts', ['title' => 'Articles in: '. $category->name, 'posts' => $category->posts]);
 });
 
 Route::get('/contact', function () {
